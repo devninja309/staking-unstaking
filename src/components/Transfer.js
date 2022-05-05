3c0a833689c21317642feb7ab2e4dbd23006845c
ryooyama <ryooyama.dev@gmail.com>
2022-05-04 19:27:12 -0700

    no refresh

--- src/components/Transfer.js
+++ src/components/Transfer.js
@@ -15,6 +15,7 @@ const Insurance = () => {
                     <button className="title-btn">
                       <span className="icon">
                         <i className="fas fa-arrow-left"></i>
+                        {/* <></> */}
                       </span>
                     </button>
                   </Link>
